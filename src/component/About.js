f09a07a00acef96cbfa7d86f8d65f40055a75a53
Kaddrick <77331620+Kaddrick@users.noreply.github.com>
2022-04-08 13:27:46 +0100

    Update About.js

--- src/component/About.js
+++ src/component/About.js
@@ -9,18 +9,18 @@ export default function About() {
                 <img src={profile} className="h-52 rounded-full mx-auto m-4"/>
                 <h1 className="text-white text-3xl cursive m-4">Who am i?</h1>
                 <p className="text-white cursive m-4">
-                Born in France and moved to the UK in 2013, i have worked for the past 8 years in hospitality industry
-                    during the second lockdown i decided it was time for a change. I decided to learn coding, i started with
-                    a free app then moved to tutorial and practice. Then in February and March i passed two MTAs to validate
+                Born in France and moved to the UK in 2013, i have worked for the past 8 years in hospitality industry and 
+                    during the second lockdown i decided it was time for a change. I learn to code, i started with
+                    a free app then moved to tutorial and practice. Then in February and March i passed two MTAs (Microsoft Technology Associate) to validate
                     my skills. 
                 </p>
                 <p className="text-white cursive m-4">
-                I I am currently as well a Dungeon Master for my group of friends playing Dungeon and Dragons. 
-                This is one of my hobby i have been able to pursue despite the current pandemic, we play via zoom
-                and it inspired my first big project (see the spell sheet on the project page).
+                I am currently as well a Dungeon Master for my group of friends playing Dungeon and Dragons. 
+                This is one of my hobby i have been able to pursue despite the current pandemic, we play online
+                and it inspired my first project (see the spell sheet on the project page).
                 </p>
                 <p className="text-white cursive m-4">
-                    Another hobby of mine is travelling i have travelled through most of Europe but also outside of it.
+                    Another hobby of mine is travelling, i have travelled through most of Europe but also outside of it.
                     I have been lucky enough to go to Egypt see the pyramids, to Norway in the Arctic circle and see the northern
                     lights, climb to the Tiger Cave Temple in Krabi in Thailand. All amazing memories and i definitely looking
                     forward to make more.
