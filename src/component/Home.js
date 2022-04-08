35ce4f6a99ddffc99801cc14a0b4eb6a05ab6a4b
Kaddrick <77331620+Kaddrick@users.noreply.github.com>
2022-04-08 13:18:00 +0100

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
         <main>
             <img src={background} alt="background" className="absolute object-cover w-full h-full"/>
             <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-                <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-snug hone-name">Hi, i'm Cedric web developer.</h1>
+                <h1 className="text-6xl text-white font-bold cursive leading-none lg:leading-snug hone-name">Hi, i'm Cedric</h1>
             </section>
         </main>
     )
